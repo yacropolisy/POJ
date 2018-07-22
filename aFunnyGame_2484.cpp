94eab596f7c85dbf5f418abf83763d5af07817b1
Yoshinobu Yamada <cancer_acropolis@yahoo.co.jp>
2018-07-22 10:27:13 +0900

    2484 AC

---
+++ aFunnyGame_2484.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <map>
+#include <deque>
+#include <cstring>
+using namespace std;
+
+#define REP(i,n) for(int i = 0; i < n; i++)
+#define ALL(v) v.begin(),v.end()
+typedef long long ll;
+typedef pair<int,int> pi;
+typedef pair<ll,ll> pll;
+typedef pair<string,string> pss;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pi> vpi;
+typedef vector<ll> vll;
+typedef vector<vll> vvll;
+
+double EPS = 1e-9;
+int INFi = 1000000005;
+long long INFll = 1000000000000000005ll;
+double PI = acos(-1);
+int dirx[8] = {-1,0,0,1,-1,-1,1,1};
+int diry[8] = {0,1,-1,0,-1,1,-1,1};
+ll MOD = 1000000007;
+
+const int MAX_N = 10000;
+
+
+int main(){
+  vi ns;
+  int n;
+  while(1){
+    cin >> n;
+    if(n == 0) break;
+    ns.push_back(n);
+  }
+
+  REP(i, ns.size()){
+    cout << (ns[i] < 3 ? "Alice" : "Bob") << endl; 
+  }
+
+  return 0;
+}
